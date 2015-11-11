8b25f16242cd3dd6c71bd4370ba280b9b51d86f7
maximenathan <maxime.soufflet@epitech.eu>
2015-11-11 17:14:26 +0100

    Update Playlist : erreurs

--- WpfApplication1/ViewModel/ViewModelWMP.cs
+++ WpfApplication1/ViewModel/ViewModelWMP.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Windows;
 using System.Windows.Input;
 using System.Xml;
 using System.Xml.Serialization;
@@ -290,29 +291,60 @@ class ViewModelWMP : BaseModel
         Tuple<Playlist, Media> parameters = (Tuple<Playlist, Media>)parameter;
 
         String nameList = (String)parameters.Item1.Index;
-        // String nameNewMedia = (String)parameter.Item2;
         XmlSerializer deserializerPlaylist = new XmlSerializer(typeof(Playlist));
         Environment.CurrentDirectory = @"C:\";
-        try
+        using (Stream reader = new FileStream(RootRepo + Environment.UserName + ProjectRepo + "\\" + nameList, FileMode.Open))
         {
-            using (Stream reader = new FileStream(RootRepo + Environment.UserName + ProjectRepo + "\\" + nameList, FileMode.Open))
+            try
             {
                 _selectedPlaylist = (Playlist)deserializerPlaylist.Deserialize(reader);
                 reader.Close();
                 _selectedPlaylist.Add(parameters.Item2);
                 if (File.Exists(RootRepo + Environment.UserName + ProjectRepo + "\\" + nameList))
                 {
+
                     File.Delete(RootRepo + Environment.UserName + ProjectRepo + "\\" + nameList);
                     XmlSerializer serializePlaylist = new XmlSerializer(typeof(Playlist));
                     TextWriter WriteFileStream = new StreamWriter(RootRepo + Environment.UserName + ProjectRepo + "\\" + nameList);
-                    serializePlaylist.Serialize(WriteFileStream, _selectedPlaylist);
-                    WriteFileStream.Close();
+                    try
+                    {
+                        serializePlaylist.Serialize(WriteFileStream, _selectedPlaylist);
+                        WriteFileStream.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBoxResult result = MessageBox.Show("Erreur lors de l'ajout du média", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            Application.Current.Shutdown();
+                        }
+                    }
+                    finally
+                    {
+                        WriteFileStream.Close();
+                    }
                 }
             }
-        }
-        catch (Exception e)
-        {
-
+            catch (InvalidOperationException e)
+            {
+                MessageBoxResult result = MessageBox.Show("Erreur lors de la lécture de la playlist", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBoxResult result = MessageBox.Show("Erreur", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+            finally
+            {
+                
+            }
         }
     }
 
